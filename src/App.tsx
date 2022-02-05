e689690b1652e7b5bce85693e3e5f0f9186d47bf
Jelaque <jesuslazoquevedo@gmail.com>
2022-02-04 23:40:26 -0500

    Project Init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Form from './components/companyForm';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container center-block">
+        <Form />
+      </div>
+    </div>
+  );
+}
+
+export default App;
